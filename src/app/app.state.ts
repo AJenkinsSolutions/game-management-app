5478ad1a7a71a600c253d74225478ba68be626db
AJenkinsSoultions <ajenkinsuiw@gmail.com>
2024-02-20 15:57:49 -0700

    Add: readOnly Entity to app state interface

--- src/app/app.state.ts
+++ src/app/app.state.ts
@@ -1,2 +1,8 @@
+import { Game } from "./models/game";
+
 export interface AppState {
+
+    //Because the app state is immutable we need to specify readonly or our game entity
+    // ** Even though this is an array the convention is to still name it singularly 
+    readonly game: Game[]
 }
