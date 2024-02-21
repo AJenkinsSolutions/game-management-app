4c9abd03819f179c02c674765a24669bc4783436
AJenkinsSoultions <ajenkinsuiw@gmail.com>
2024-02-21 10:30:22 -0700

    Add: Remove game actions

--- src/app/games/game.actions.ts
+++ src/app/games/game.actions.ts
@@ -2,3 +2,10 @@ import { createAction, props } from "@ngrx/store";
 import { Game } from "../models/game";
 
 //point to actaul things you should create. like the components that make up the actions
+
+//1. export consts
+//specific the type 
+//Add props,  in this case its a Game
+export const AddGame = createAction('[Game] Add Game', props<Game>()); 
+
+export const RemoveGame = createAction('[Game] Add Game', props<{gameId: string}>());
