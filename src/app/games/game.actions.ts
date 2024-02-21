a7a9ea165cdaa103da368cf909c77d8f022aab32
AJenkinsSoultions <ajenkinsuiw@gmail.com>
2024-02-21 10:26:30 -0700

    Add: Book model to actions

--- src/app/games/game.actions.ts
+++ src/app/games/game.actions.ts
@@ -1,1 +1,4 @@
 import { createAction, props } from "@ngrx/store";
+import { Game } from "../models/game";
+
+//point to actaul things you should create. like the components that make up the actions
