3bbd204f73be8e7eb8ce34c41c393d64e5b7fcac
Alex Jenkins <ajenkinsuiw@gmail.com>
2024-02-24 21:34:22 -0600

    Add: Creating store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { StoreModule } from '@ngrx/store';
+import { GameReducer } from './games/game.reducer';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({game : GameReducer})
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
